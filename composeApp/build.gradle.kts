0795d82aecd97bbe6fbac37633191f2e2af8d30d
Ion Varanita <ion.varanita@alkemy.com>
2025-11-16 06:19:44 +0200

    Fix date and time android Api < 26

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -127,10 +127,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        isCoreLibraryDesugaringEnabled = true
     }
 
     dependencies {
         debugImplementation(libs.compose.ui.tooling)
+        coreLibraryDesugaring(libs.desugar.jdk.libs)
     }
 }
 
